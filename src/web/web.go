a01a7b8f5fb0686d7acdd63a0cce1f7d1b2319a6
luojian <305170891@qq.com>
2017-09-27 10:48:27 +0800

    revert

--- src/web/web.go
+++ src/web/web.go
@@ -39,8 +39,8 @@ const templateStr = `
 <br>
 {{end}}
 <form action="/" name=f method="GET"><input maxLength=1024 size=70
-name=s value=""  title="Text to QR Encode"><input type=submit 
-value="Show QR" name=qr>1
+name=s value="" title="Text to QR Encode"><input type=submit
+value="Show QR" name=qr>
 </form>
 </body>
 </html>
